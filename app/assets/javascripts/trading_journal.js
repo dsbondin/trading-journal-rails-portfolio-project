ca911d5fa30abf68576f836ba79b436948b8052f
github username <github email address>
2018-01-16 20:46:22 -0500

    refactored renderComments, handled no comments situation

--- app/assets/javascripts/trading_journal.js
+++ app/assets/javascripts/trading_journal.js
@@ -85,14 +85,21 @@ const renderEmptyTrade = function(json) {
 const loadComments = function() {
   $("button#show-comments").click(function() {
     let trade_id = $(this).attr("trade-id")
-    $.getJSON("/trades/" + trade_id + "/comments").success(function(comments) {
-      // console.log(comments)
-      let commentsHTML = ""
-      comments.forEach(function(comment) {
-        commentsHTML += "<p>" + comment.trader.email + "<br>" + comment.body + "</p>"
-        $("#comments").html(commentsHTML)
-        $("button#show-comments").hide()
-      })
+    $.getJSON("/trades/" + trade_id + "/comments").done(function(comments) {
+      renderComments(comments);
     })
   })
+}
+
+const renderComments = function(comments) {
+  if (comments.length > 0) {
+    let commentsHTML = ""
+    comments.forEach(function(comment) {
+      commentsHTML += "<p>" + comment.trader.email + "<br>" + comment.body + "</p>";
+      $("#comments").html(commentsHTML);
+    })
+  } else {
+    $("#comments").text("No comments yet");
+  }
+  $("button#show-comments").hide();
 }
