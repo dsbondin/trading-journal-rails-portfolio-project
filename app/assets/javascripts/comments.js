396e31c23c17e372c229768030b70cc2b19b4af4
github username <github email address>
2018-01-16 21:47:51 -0500

    added Add Comment button

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,22 @@
+const loadComments = function() {
+  $("button#show-comments").click(function() {
+    let trade_id = $(this).attr("trade-id")
+    $.getJSON("/trades/" + trade_id + "/comments").done(function(comments) {
+      renderComments(comments);
+    })
+  })
+}
+
+const renderComments = function(comments) {
+  if (comments.length > 0) {
+    let commentsHTML = ""
+    comments.forEach(function(comment) {
+      commentsHTML += "<p>" + comment.trader.email + "<br>" + comment.body + "</p>";
+      $("#comments").html(commentsHTML);
+    })
+  } else {
+    $("#comments").text("No comments yet");
+  }
+  $("button#show-comments").hide();
+  $("button#add-comment").show();
+}
--- app/assets/javascripts/trading_journal.js
+++ app/assets/javascripts/trading_journal.js
@@ -89,25 +89,3 @@ const renderEmptyTrade = function(json) {
   $(".hide-when-no-trade").hide();
   $(".trade_id").attr("trade-id", json.id);
 }
-
-const loadComments = function() {
-  $("button#show-comments").click(function() {
-    let trade_id = $(this).attr("trade-id")
-    $.getJSON("/trades/" + trade_id + "/comments").done(function(comments) {
-      renderComments(comments);
-    })
-  })
-}
-
-const renderComments = function(comments) {
-  if (comments.length > 0) {
-    let commentsHTML = ""
-    comments.forEach(function(comment) {
-      commentsHTML += "<p>" + comment.trader.email + "<br>" + comment.body + "</p>";
-      $("#comments").html(commentsHTML);
-    })
-  } else {
-    $("#comments").text("No comments yet");
-  }
-  $("button#show-comments").hide();
-}
