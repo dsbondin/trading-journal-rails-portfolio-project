fe15a9333579a32cf8d1f60ad8d419435d8bbb4b
github username <github email address>
2018-02-20 21:33:15 -0500

    deploying to heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,13 +250,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  FACEBOOK_KEY=1907091676275677
-  FACEBOOK_SECRET=1aa2dee678f340d0d48a0319f878bedb
-  GITHUB_KEY=fd73367ceb06386681de
-  GITHUB_SECRET=7ec712d3970523a1651cd903bc1c93e53e90965b
 
-  config.omniauth :github, GITHUB_KEY, GITHUB_SECRET, :scope => 'user:email'
-  config.omniauth :facebook, FACEBOOK_KEY, FACEBOOK_SECRET
+  config.omniauth :github, fd73367ceb06386681de, 7ec712d3970523a1651cd903bc1c93e53e90965b, :scope => 'user:email'
+  config.omniauth :facebook, 1907091676275677, 1aa2dee678f340d0d48a0319f878bedb
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
